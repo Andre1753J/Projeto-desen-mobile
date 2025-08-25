e01e107e8b002f2ef347da42245274eb7b0df6ee
Andre1753J <andreorocondo@gmail.com>
2025-08-25 17:34:44 -0400

    gato

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import { useState, useEffect } from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Pam Pam Pam... PAAAAAAAAM</Text>
+      <TextInput style={styles.input} placeholder='escreva algo' placeholderTextColor={"#202"} />
+      <Image source={require("./gato-explod.gif")}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "#999",
+    fontSize: 25
+  },
+  input: {
+    backgroundColor: "#999",
+    width: 200,
+    borderRadius: 20
+  }
 });
+
+// View == div && section
+//Button == button
+//Text == p
